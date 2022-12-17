945a0aadacf79c0b8d7f7e1954187fe39abd8eba
Daniyar Turak <dany.turak@gmail.com>
2022-12-17 13:13:37 +0600

    feat: mobile version

--- src/app/shared/modules/header/header.module.ts
+++ src/app/shared/modules/header/header.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
