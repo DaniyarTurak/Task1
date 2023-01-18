474bcf934066adb00f84146733925f1410045575
Daniyar Turak <dany.turak@gmail.com>
2023-01-18 10:16:02 +0600

    fix: promo layout text length

--- src/app/home/components/promo/promo.component.ts
+++ src/app/home/components/promo/promo.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class PromoComponent {
   showMore: boolean = false;
   text: string =
-    'We do something really useful, important, and unique. Learn all about it here';
+    'We do something really useful, important, and unique. Learn all about it here Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique architecto deserunt consectetur molestias o!';
 
   setShowMore(): void {
     this.showMore = !this.showMore;
