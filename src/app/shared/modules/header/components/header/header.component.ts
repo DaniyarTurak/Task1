f664a11685e712afb39beb124a3fcac01ebcf258
Daniyar Turak <dany.turak@gmail.com>
2023-04-10 18:58:56 +0600

    fix bugs

--- src/app/shared/modules/header/components/header/header.component.ts
+++ src/app/shared/modules/header/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mc-header',
@@ -6,9 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  routes = [
+    {
+      path: '',
+      text: 'Homepage',
+    },
+    {
+      path: 'three-columns',
+      text: 'Three Columns',
+    },
+    {
+      path: 'two-column-1',
+      text: 'Two Column #1',
+    },
+    {
+      path: 'two-column-2',
+      text: 'Two Column #2',
+    },
+    {
+      path: 'one-column',
+      text: 'One Column',
+    },
+  ];
+  currentRoute: string = '';
   checkboxFlag: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentRoute = this.router.url;
+  }
+
+  handleClick(path: string): void {
+    this.currentRoute = path;
+  }
+
   closeSideBar() {
-    this.checkboxFlag = false;
+    this.checkboxFlag = !this.checkboxFlag;
   }
 }
